211fd8e663e0fd15319a36682e135cd44c0e3456
Stefan <stefang326@gmail.com>
2024-03-08 10:13:31 +0100

    Create Terminfindungsapp

---
+++ Terminfindungsapp/LoginWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Terminfindungsapp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if(username.Count()>4 && password.Count() > 8)
+            {
+                //TODO: Request to API-Server
+                
+                MainWindow window = new MainWindow();
+                window.Show();
+                this.Close();
+            }
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow register = new RegisterWindow();
+            register.Show();
+            this.Close();
+        }
+    }
+}
